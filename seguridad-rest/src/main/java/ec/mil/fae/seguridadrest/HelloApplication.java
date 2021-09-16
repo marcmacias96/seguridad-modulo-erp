92e986a9163f050973ce498ea537b62c74c101bd
marcmacias96 <mamacias3@espe.edu.ec>
2021-09-16 11:37:56 -0500

    change proyect

---
+++ seguridad-rest/src/main/java/ec/mil/fae/seguridadrest/HelloApplication.java
@@ -0,0 +1,9 @@
+package ec.mil.fae.seguridadrest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
