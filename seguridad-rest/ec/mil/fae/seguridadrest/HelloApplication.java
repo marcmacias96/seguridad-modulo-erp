da6fdf125979ac320046e446ebe0991da79c62c6
Alex <agespinosa1@espe.edu.ec>
2021-09-21 13:44:06 -0500

    cambios

--- seguridad-rest/ec/mil/fae/seguridadrest/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package ec.mil.fae.seguridadrest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
